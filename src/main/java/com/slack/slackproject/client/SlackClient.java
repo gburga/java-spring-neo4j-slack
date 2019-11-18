a1ea748b3204063990954f9f2cac43251bf82826
Giulio Burga <giulio.burga@intelygenz.com>
2019-11-18 16:46:24 +0100

    first approach

---
+++ src/main/java/com/slack/slackproject/client/SlackClient.java
@@ -0,0 +1,12 @@
+package com.slack.slackproject.client;
+
+import com.hubspot.slack.client.SlackClientModule;
+
+public class SlackClient {
+/*    public class MyFancyModule extends AbstractModule {
+        @Override
+        protected void configure() {
+            install(new SlackClientModule());
+        }
+    }*/
+}
---
+++ src/test/java/com/slack/slackproject/SlackProjectApplicationTests.java
@@ -0,0 +1,14 @@
+package com.slack.slackproject;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
